3b3006426875ed5e2ee55aeebc7d400fe2488ccb
timow-gh <39085531+timow-gh@users.noreply.github.com>
2021-12-27 17:23:23 +0100

    Cleaned up PlacingInteractor.cpp

--- flowmesh/include/FlowMesh/Interactors/PlacingInteractor.hpp
+++ flowmesh/include/FlowMesh/Interactors/PlacingInteractor.hpp
@@ -3,15 +3,15 @@
 
 #include <Core/Types/TVector.hpp>
 #include <Core/Utils/Assert.hpp>
-#include <GraphicsInterface/InputEvents/InputEventListener.hpp>
-#include <GraphicsInterface/InputEvents/PickRayEvent.hpp>
-#include <GraphicsInterface/InputEvents/PickRayMoveEvent.hpp>
-#include <GraphicsInterface/InputEvents/RayPickEventListener.hpp>
 #include <FlowMesh/FlowMeshGuid.hpp>
 #include <FlowMesh/FlowMeshModel.hpp>
 #include <FlowMesh/Interactors/Interactor.hpp>
 #include <Geometry/Plane.hpp>
 #include <Geometry/Ray.hpp>
+#include <GraphicsInterface/InputEvents/InputEventListener.hpp>
+#include <GraphicsInterface/InputEvents/PickRayEvent.hpp>
+#include <GraphicsInterface/InputEvents/PickRayMoveEvent.hpp>
+#include <GraphicsInterface/InputEvents/RayPickEventListener.hpp>
 #include <optional>
 
 namespace FlowMesh
@@ -21,51 +21,18 @@ class PlacingInteractor : public Interactor {
     std::optional<FGuid> m_sphereGuid;
 
   public:
-    explicit PlacingInteractor(FlowMeshModel* model) : m_model(model) {}
+    explicit PlacingInteractor(FlowMeshModel* model) CORE_NOEXCEPT;
+    void ~PlacingInteractor() CORE_NOEXCEPT;
 
   private:
-    CORE_NODISCARD FlowMeshSphere createSphere(const LinAl::Vec3d& origin) const
-    {
-        constexpr double_t radius = 0.2;
-        auto sphere = Geometry::Sphere<double_t>{origin, radius};
-        return FlowMeshSphere{sphere, newFGuid()};
-    }
+    CORE_NODISCARD FlowMeshSphere
+    createSphere(const LinAl::Vec3d& origin) const;
 
     CORE_NODISCARD std::optional<LinAl::Vec3d>
-    calcIntersection(const Graphics::PickRayEvent& pickRayEvent) const
-    {
-        const Graphics::Vec3& pickOrigin = pickRayEvent.origin;
-        const Graphics::Vec3& pickDirection = pickRayEvent.direction;
-        const Geometry::Ray3<double_t> ray{
-            LinAl::Vec3d{pickOrigin[0], pickOrigin[1], pickOrigin[2]},
-            LinAl::Vec3d{pickDirection[0], pickDirection[1], pickDirection[2]}};
-        const Geometry::Plane plane{LinAl::ZERO_VEC3D, LinAl::Z_VEC3D};
-        return plane.intersection(ray);
-    }
+    calcIntersection(const Graphics::PickRayEvent& pickRayEvent) const;
 
-    void event(const Graphics::PickRayEvent& pickRayEvent) override
-    {
-        if (auto intersection = calcIntersection(pickRayEvent))
-        {
-            FlowMeshSphere flowMeshSphere = createSphere(*intersection);
-            m_model->add(flowMeshSphere);
-        }
-    }
-
-    void event(const Graphics::PickRayMoveEvent& pickRayMoveEvent) override
-    {
-        if (auto intersection = calcIntersection(pickRayMoveEvent))
-        {
-            if (!m_sphereGuid)
-            {
-                FlowMeshSphere flowMeshSphere = createSphere(LinAl::ZERO_VEC3D);
-                m_model->add(flowMeshSphere);
-                m_sphereGuid = flowMeshSphere.getFGuid();
-                return;
-            }
-            m_model->setPosition(*m_sphereGuid, *intersection);
-        }
-    }
+    void event(const Graphics::PickRayEvent& pickRayEvent) override;
+    void event(const Graphics::PickRayMoveEvent& pickRayMoveEvent) override;
 };
 
 } // namespace FlowMesh
