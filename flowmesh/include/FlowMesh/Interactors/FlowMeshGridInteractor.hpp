b9cefabba8ca2cbd1b4534d8a2b6281cd1d0b11b
timow-gh <39085531+timow-gh@users.noreply.github.com>
2022-05-16 20:54:17 +0200

    FlowMeshGridInteractor sets the new minimum size when objects are placed.

--- flowmesh/include/FlowMesh/Interactors/FlowMeshGridInteractor.hpp
+++ flowmesh/include/FlowMesh/Interactors/FlowMeshGridInteractor.hpp
@@ -3,6 +3,7 @@
 
 #include <FlowMesh/FlowMeshModel.hpp>
 #include <FlowMesh/GeometryElements/FlowMeshGrid.hpp>
+#include <Geometry/Plane.hpp>
 #include <GraphicsInterface/InputEvents/RayPickEventListener.hpp>
 
 namespace FlowMesh
@@ -12,16 +13,22 @@ class FlowMeshGridInteractor : public Graphics::RayPickEventListener {
     FlowMeshModel* m_model{nullptr};
     double_t m_updateDistance{6.0};
     double_t m_diffUpdateDistance{2.0};
+    Geometry::Plane<double_t> m_plane;
     FlowMeshGrid m_minimalGrid{};
     LinAl::Vec3d m_pevGridIntersectionPoint{0, 0, 0};
-    FGuid m_gridGuid;
+    FGuid m_activeGridGuid;
 
   public:
     static std::unique_ptr<FlowMeshGridInteractor> create(FlowMeshModel& model,
                                                           const FlowMeshGrid& grid);
 
   private:
-    FlowMeshGridInteractor(FlowMeshModel& model, const FGuid& fGuid);
+    FlowMeshGridInteractor(FlowMeshModel& model,
+                           Geometry::Plane<double_t> plane,
+                           const FGuid& fGuid);
+
+    [[nodiscard]] std::optional<LinAl::Vec3d>
+    rayFromEvent(const Graphics::PickRayEvent& pickRayEvent) const;
 
     void onEvent(const Graphics::PickRayEvent& pickRayEvent) override;
     void onEvent(const Graphics::PickRayMoveEvent& pickRayMoveEvent) override;
